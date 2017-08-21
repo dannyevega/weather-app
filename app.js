f14b7d3102f8c175db93ae16a7dae1e8c5e77638
Danny Vega <dannyevega@gmail.com>
2017-08-20 18:08:35 -0700

    formatted date and added another route to pass in how many days you want to check

--- app.js
+++ app.js
@@ -12,15 +12,23 @@ weatherApp.controller('mainController', ['$scope', 'cityService', function($scop
         
 }]);
 
-weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService', function($scope, $resource, cityService){
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService){
     
     $scope.city = cityService.city;    
     
+    $scope.days = $routeParams.days || 5;
+    
     $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=5e3f1215e6dc1f45f3eb32bb6087fbbb", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP"}});
     
-    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: 5 });
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
     
-    console.log($scope.weatherResult);
+    $scope.convertToDate = function(dt){
+        return new Date(dt * 1000);
+    }
+    
+    $scope.convertToFahrenheit = function(kelvin){
+        return Math.round((1.8 * (kelvin - 273)) + 32);
+    }    
     
 }]);
 
@@ -35,6 +43,11 @@ weatherApp.config(function($routeProvider){
     })
     
     .when('/forecast', {
+        templateUrl: 'pages/forecast.html',
+        controller: 'forecastController'
+    })    
+    
+    .when('/forecast/:days', {
         templateUrl: 'pages/forecast.html',
         controller: 'forecastController'
     })    
