8a51800114fd90753106e0c17e11e6f88731adda
Danny Vega <dannyevega@gmail.com>
2017-08-22 13:06:02 -0700

    moved around html files and added ng-submit to form-group so user can press enter key and the button

--- js/controllers.js
+++ js/controllers.js
@@ -1,11 +1,15 @@
 // CONTROLLERS
-weatherApp.controller('mainController', ['$scope', 'cityService', function($scope, cityService){
+weatherApp.controller('mainController', ['$scope', '$location', 'cityService',function($scope, $location, cityService){
     
     $scope.city = cityService.city;
     
     $scope.$watch('city', function(){
        cityService.city = $scope.city; 
     });
+    
+    $scope.submit = function(){
+        $location.path("/forecast");
+    }
         
 }]);
 
